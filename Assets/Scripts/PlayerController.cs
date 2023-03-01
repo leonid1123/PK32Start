93966fcff3810ec237faf02dc174f074c7bee3f5
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-03-01 13:36:58 +0300

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    public float spd=2f;
+    [SerializeField]
+    float jumpForce = 1f;
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
@@ -13,6 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb2d.AddRelativeForce(new Vector2(Input.GetAxisRaw("Horizontal"),0),ForceMode2D.Impulse);
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*spd,rb2d.velocity.y);
+        if(Input.GetButtonUp("Jump")) {
+            rb2d.AddRelativeForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+        }
     }
 }

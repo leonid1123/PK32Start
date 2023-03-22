b882cde04a8668a3242ad8ca5f3738ca1f61b92b
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-03-22 13:30:12 +0300

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,47 @@ public class PlayerController : MonoBehaviour
     public float spd=2f;
     [SerializeField]
     float jumpForce = 1f;
+    Animator anim;
+    bool isRight = true;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    float groundCheckRadius=0.1f;
+    [SerializeField]
+    LayerMask groundMask;
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*spd,rb2d.velocity.y);
-        if(Input.GetButtonUp("Jump")) {
-            rb2d.AddRelativeForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("spd",Mathf.Abs(moveInput));
+        rb2d.velocity = new Vector2(moveInput*spd,rb2d.velocity.y);
+        if(isRight && moveInput<0){
+            Flip();
         }
+        if(!isRight && moveInput>0){
+            Flip();
+        }
+        if(Input.GetButtonUp("Jump")) {
+            Collider2D isground = Physics2D.OverlapCircle(new Vector2(groundCheck.position.x,groundCheck.position.y),groundCheckRadius,groundMask);
+            if(isground!=null && isground.CompareTag("Ground"))
+            {
+                rb2d.AddRelativeForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+                anim.SetTrigger("isJump");
+            }
+        }
+    }
+    void Flip() {
+        transform.Rotate(0,180,0);
+        isRight = !isRight;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position,groundCheckRadius);
     }
 }
